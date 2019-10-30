8d242a7d97f965f3dc55e3dee409c19b49b421a2
cdhbze93 <antoebt@hotmail.com>
2019-10-30 15:08:13 +0100

    add: Router/LoadPage

--- Hallowen24H/src/app/app-routing.module.ts
+++ Hallowen24H/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { AccueilComponent } from './components/accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoadpageComponent } from './components/loadpage/loadpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "accueil", component: AccueilComponent},
+  {path: "", component: LoadpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
