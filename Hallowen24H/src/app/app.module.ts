006a50d1ef63e89506f79fa4ea9e38c9de026e4c
cdhbze93 <antoebt@hotmail.com>
2019-10-30 14:28:22 +0100

    add: Components

--- Hallowen24H/src/app/app.module.ts
+++ Hallowen24H/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadpageComponent } from './components/loadpage/loadpage.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ChoixMonstreComponent } from './components/choix-monstre/choix-monstre.component';
+import { BattlegroundComponent } from './components/battleground/battleground.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { ContainerBarreComponent } from './components/container-barre/container-barre.component';
+import { HUDComponent } from './components/hud/hud.component';
+import { MonstresComponent } from './components/monstres/monstres.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadpageComponent,
+    AccueilComponent,
+    ChoixMonstreComponent,
+    BattlegroundComponent,
+    TimerComponent,
+    ContainerBarreComponent,
+    HUDComponent,
+    MonstresComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Hallowen24H/src/app/components/choix-monstre/choix-monstre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choix-monstre',
+  templateUrl: './choix-monstre.component.html',
+  styleUrls: ['./choix-monstre.component.scss']
+})
+export class ChoixMonstreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
