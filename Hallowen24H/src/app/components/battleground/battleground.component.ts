188cf22faa4851029c593ce21cbcb4f76b75d3f6
cdhbze93 <antoebt@hotmail.com>
2019-10-31 05:19:57 +0100

    feat: ajout skill

--- Hallowen24H/src/app/components/battleground/battleground.component.ts
+++ Hallowen24H/src/app/components/battleground/battleground.component.ts
@@ -1,3 +1,4 @@
+import { AttackService } from './../../service/attack.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { MonstreService } from 'src/app/service/monstre.service';
 
@@ -8,23 +9,41 @@ import { MonstreService } from 'src/app/service/monstre.service';
 })
 export class BattlegroundComponent implements OnInit {
 
-
-  public persoB = {}
-
-  @Input() persoA;
-  @Input() test;
+  public attacks = [];
+  public persoB = "";
   public playerOne;
-
-
-  constructor(private _monstreService: MonstreService) { 
+  public getRandomInt() {
+    return Math.floor(Math.random() * Math.floor(this._monstreService.allMonster.length));
   }
-  
-  ngOnInit() {    
-   this.playerOne = this._monstreService.playerOneImg;
+  public checker() {
+    if (this.playerOne === "../../../assets/json.gif") {
+      return 1;
+    } else if (this.playerOne === "../../../assets/alucard.gif") {
+      return 2;
+    } else {
+      return 6;
+    }
+  }
+  bootAttack(idMonster: number) {
+    for (let i = 0; i < this._attackService.allAttack.length; i++) {
+      if (this._attackService.allAttack[i].idMonster === idMonster) {
+        this.attacks.push(this._attackService.allAttack[i]);
+      }
+    }
+    console.log(this.attacks);
+    return this.attacks;
+  }
+  constructor(private _monstreService: MonstreService, private _attackService: AttackService) {
   }
 
-  test2(){
-    console.log(this.playerOne + "JE SUIS P1");
+  ngOnInit() {
+    this.getRandomInt();
+    this.playerOne = this._monstreService.playerOneImg;
+    this.persoB = this._monstreService.allMonster[this.getRandomInt()].image;
+    this.checker();
+    console.log(this.checker());
+    this.bootAttack(this.checker());
   }
 
 }
+
