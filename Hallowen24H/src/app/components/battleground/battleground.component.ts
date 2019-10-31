731c0cf38aba22601101d9f25baeb3228552360f
cdhbze93 <antoebt@hotmail.com>
2019-10-31 07:22:47 +0100

    fix: battleground

--- Hallowen24H/src/app/components/battleground/battleground.component.ts
+++ Hallowen24H/src/app/components/battleground/battleground.component.ts
@@ -1,6 +1,7 @@
 import { AttackService } from './../../service/attack.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { MonstreService } from 'src/app/service/monstre.service';
+import { link } from 'fs';
 
 @Component({
   selector: 'app-battleground',
@@ -9,8 +10,8 @@ import { MonstreService } from 'src/app/service/monstre.service';
 })
 export class BattlegroundComponent implements OnInit {
 
-  public life1 = 50;
-  public life2 = 70;
+  public life1 = 100;
+  public life2 = 100;
 
   public attacks = [];
   public persoB = "";
@@ -27,18 +28,32 @@ export class BattlegroundComponent implements OnInit {
       return 42;
     }
   }
-  bootAttack(idMonster: number) {
+  public linkLife() {
+    document.getElementById("currentLife1").style.width = `${this.life1}%`;
+    document.getElementById("currentLife2").style.width = `${this.life2}%`;
+  }
+  public bootAttack(idMonster: number) {
     for (let i = 0; i < this._attackService.allAttack.length; i++) {
       if (this._attackService.allAttack[i].idMonster === idMonster) {
         this.attacks.push(this._attackService.allAttack[i]);
       }
     }
-    console.log(this.attacks);
+    console.log('je suis  attacks' + this.attacks);
     return this.attacks;
   }
 
-  public test(){
-    this.life1 -= 30;
+  public atkP1(x) {
+    this.attacks[x];
+    if (x === 0) {
+      this.life2 -= this.attacks[x].damage
+    } else if (x === 1) {
+      this.life2 -= this.attacks[x].damage
+    } else if (x === 2) {
+      this.life2 -= this.attacks[x].damage
+    } else if (x === 3) {
+      this.life2 -= this.attacks[x].damage
+    }
+    this.linkLife();
   }
   constructor(private _monstreService: MonstreService, private _attackService: AttackService) {
   }
@@ -53,10 +68,6 @@ export class BattlegroundComponent implements OnInit {
     this.linkLife();
   }
 
-  linkLife(){
-    const L1 = document.getElementById("currentLife1").style.width = `50%`;
-    const L2 = document.getElementById("currentLife2").style.width = `{life2}%`;
-  }
 
 }
 
